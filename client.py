b2197f3c9b9178edecd1a9dc1b008162a82682f9
ZloyenotVN <gluhov.oleg.v.yandex.ru>
2022-06-30 14:40:08 +0300

    update2 client.py

--- client.py
+++ client.py
@@ -1,14 +1,27 @@
 import websocket
 import json
+import time
+import asyncio
+
+
+async def dialog(ws,x,N=100):
+    for i in range(N):
+        x['data'] = {"UWB": [0, 1, i]}
+        await ws.send(f"{json.dumps(x)}")
 
 def on_message(ws, message):
+    print('prishlo')
     data = json.loads(message)
     print('Сообщение')
-    print(data)
+    print(message)
+    if data["action"] == "Login":
+        dialog(ws,x=massIN)
+        print('Success')
 
 def on_error(ws, error):
-    print("Ошибка")
-    print(error)
+    # print("Ошибка")
+    # print(error)
+    pass
 
 
 def on_close(ws):
@@ -17,18 +30,17 @@ def on_close(ws):
 
 
 def on_open(ws):
-    # ws.send(json.dumps([0,1,2,3,4,5]))
-    ws.send("{\"action\":\"Login\"}")
+    ws.send('{"action":"Login","login":"mathName"}')
+
 
 if __name__ == "__main__":
-
-
+    massIN = {"action":"Send2Math","data":{"UWB": [0, 1, 2]}}
     websocket.enableTrace(True)
     input = websocket.WebSocketApp("ws://10.3.168.123:9000",
+                                on_open=on_open,
                                 on_message = on_message,
                                 on_error = on_error,
                                 on_close = on_close)
 
 
-    input.on_open = on_open
     input.run_forever()
